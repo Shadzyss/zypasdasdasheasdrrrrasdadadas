f9292106d81e363e4e4dfc93beadc7f9c59fb637
Shadzy <karataskaan0640@gmail.com>
2026-01-28 16:22:57 +0300

    envye bisiler eklendi

--- models/ticketSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ticketSchema = new mongoose.Schema({
-    guildId: { type: String, required: true },
-    userId: { type: String, required: true },
-    ticketCount: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('ticketSchema', ticketSchema);
