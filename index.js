4cef205197d6808a3f0b6fee7ed0b53b165d7734
Shadzy <karataskaan0640@gmail.com>
2026-01-28 19:49:53 +0300

    düzenlendi

--- index.js
+++ index.js
@@ -260,21 +260,4 @@ client.on('guildMemberRemove', async (member) => {
     }
 });
 
-////////// EVENTS YÜKLEME /////////
-
-// Event handler: events klasöründeki her dosyayı bir event olarak yükler
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-
 client.login(process.env.CLIENT_TOKEN);
