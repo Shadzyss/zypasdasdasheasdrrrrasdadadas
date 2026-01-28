1610db749dec7a7b1f749c45f4cd749eebaca32e
Shadzy <karataskaan0640@gmail.com>
2026-01-28 19:45:41 +0300

    ticket sistemi silindi

--- models/StaffStats.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const staffStatsSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
-    claimedTickets: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('StaffStats', staffStatsSchema);
