8d6ac5823c7ae32c4efd8b9e478210f2900b08da
Shadzy <karataskaan0640@gmail.com>
2026-01-29 03:29:22 +0300

    ticket sistemi silindi

--- models/TicketStats.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ticketStatsSchema = new mongoose.Schema({
-    userID: { type: String, required: true, unique: true }, // Yetkili ID
-    ticketCount: { type: Number, default: 0 } // Sahiplenme sayısı
-});
-
-module.exports = mongoose.model('TicketStats', ticketStatsSchema);
